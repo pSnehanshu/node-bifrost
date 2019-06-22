961126372636e31b147c2b0fa9ff204049a694e1
Snehanshu Phukon <snehanshuphukan@gmail.com>
2019-06-22 16:18:29 +0530

    scope test now doing test with two users

--- test/index.js
+++ test/index.js
@@ -30,7 +30,7 @@ describe('bifrost', function () {
 
         it('should not allow the user to perform the action on the resource', async function () {
             var result = await bifrost.allowed(user, resource, action);
-            assert.equal(result, false);
+            assert.isFalse(result);
         });
 
         it('should successfully assign the role to the user', async function () {
@@ -41,22 +41,22 @@ describe('bifrost', function () {
 
         it('should allow the user to perform the action on the resource', async function () {
             var result = await bifrost.allowed(user, resource, action);
-            assert.equal(result, true);
+            assert.isTrue(result);
         });
 
         it('should not allow the user to perform other actions on the resource', async function () {
             var result = await bifrost.allowed(user, resource, action2);
-            assert.equal(result, false);
+            assert.isFalse(result);
         });
 
         it('should not allow the user to perform the actions on other resources', async function () {
             var result = await bifrost.allowed(user, resource2, action);
-            assert.equal(result, false);
+            assert.isFalse(result);
         });
 
         it('should not allow other users to access the resource in any way', async function () {
             var result = await bifrost.allowed(user2, resource, action);
-            assert.equal(result, false);
+            assert.isFalse(result);
         });
     });
 
@@ -80,9 +80,19 @@ describe('bifrost', function () {
 
         it('should unassign the user from the role', async function () {
             await bifrost.unassign(user, role, scope);
+            await bifrost.unassign(user2, role, scope2);
+
             var userRoles = await bifrost.connector.getRolesWithScopes(user);
+            var userRoles2 = await bifrost.connector.getRolesWithScopes(user2);
+
             if (Array.isArray(userRoles[role])) {
                 assert.isFalse(userRoles[role].includes(scope));
+            } else {
+                assert.isTrue(true);
+            }
+
+            if (Array.isArray(userRoles2[role])) {
+                assert.isFalse(userRoles2[role].includes(scope2));
             } else {
                 assert.isTrue(true);
             }
@@ -90,33 +100,54 @@ describe('bifrost', function () {
 
         it('should not allow the user to perform the action on the resource', async function () {
             var result = await bifrost.allowed(user, resource, action, scope);
-            assert.equal(result, false);
+            var result2 = await bifrost.allowed(user2, resource, action, scope2);
+            assert.isFalse(result);
+            assert.isFalse(result2);
         });
 
         it('should successfully assign the role to the user', async function () {
             await bifrost.assign(user, role, scope);
+            await bifrost.assign(user2, role, scope2);
+
             var userRoles = await bifrost.connector.getRolesWithScopes(user);
+            var userRoles2 = await bifrost.connector.getRolesWithScopes(user2);
+
             assert.isTrue(userRoles[role].includes(scope));
+            assert.isTrue(userRoles2[role].includes(scope2));
         });
 
         it('should allow the user to perform the action on the resource', async function () {
             var result = await bifrost.allowed(user, resource, action, scope);
-            assert.equal(result, true);
+            var result2 = await bifrost.allowed(user2, resource, action, scope2);
+
+            assert.isTrue(result);
+            assert.isTrue(result2);
         });
 
         it('should not allow the user to perform other actions on the resource', async function () {
             var result = await bifrost.allowed(user, resource, action2, scope);
-            assert.equal(result, false);
+            var result2 = await bifrost.allowed(user2, resource, action2, scope2);
+            
+            assert.isFalse(result);
+            assert.isFalse(result2);
         });
 
         it('should not allow the user to perform the actions on other resources', async function () {
             var result = await bifrost.allowed(user, resource2, action, scope);
-            assert.equal(result, false);
+            var result2 = await bifrost.allowed(user2, resource2, action, scope2);
+
+            assert.isFalse(result);
+            assert.isFalse(result2);
         });
 
         it('should not allow other users to access the resource in any way', async function () {
             var result = await bifrost.allowed(user2, resource, action, scope);
-            assert.equal(result, false);
+            var result2 = await bifrost.allowed(user, resource, action, scope2);
+
+            assert.isFalse(result);
+            assert.isFalse(result2);
         });
+
+        
     });
 });
